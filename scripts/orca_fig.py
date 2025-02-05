1b606fe6430a8950d4e8d939a879d216fbfcb864
Sean Bergen <sbergen2@asu.edu>
2025-02-05 16:12:33 -0700

    Added basic plotting code to the python file

--- scripts/orca_fig.py
+++ scripts/orca_fig.py
@@ -9,6 +9,7 @@ then uses that dataset to create figres
 import pandas as pd
 import matplotlib.pyplot as plt
 import os
+import cmcrameri.cm as cmc
 
 # we can get the path to where scripts, orca, and figs are as follows:
 scripts_path = os.path.dirname(os.path.abspath(__file__))
@@ -21,17 +22,41 @@ orca_path = os.join(os.path.dirname(scripts_path), "ORCA")
 
 ######## figure  creation ########
 
-# data we are reading in is a csv in the format
-# [mol_name, n_edges, n_heavyatoms, ai, search_space, sgi_time, search_time, 
-#  states_searched]
+# data we are reading in is in the format
+# [mol_name, n_edges, n_heavyatoms, ai, search_space, time_to_cmp, sgi_time, 
+#  search_time, states_searched]
 #
 # mol_name    : name of the molecule
 # n_edges     : number of edges in the molecule's graph
 # n_heavyatoms: number of heavy atoms in the molecule
 # ai          : computed assembly index of the molecule
 # search_space: number of duplicate structures in the molecule
+# time_to_cmp : total time to complete a full run, averaged over x runs
 # sgi_time    : time to compute subgraph-isomorphisms, averaged over x runs
 # search_time : time to compute assembly index post sgi, averaged over x runs
 # states_searched: number of states searched when computing assembly index
 
+# at this point, we should have three pandas dataframes:
+#   - naive
+#   - logbound
+#   - seetbound
+#
+# the plot we are making is a scatter plot that shows for each of these
+# three dataframes, color of a point indicating what df it comes from
+#   x:search space
+#   y:time to compute
+fig, ax = plt.subplots(tight_layout=True)
 
+ax.scatter('search_space', 'time_to_cmp', data=naive, color=cmc.batlowS(0),
+        label="Naive")
+ax.scatter('search_space', 'time_to_cmp', data=logbound, color=cmc.batlowS(1),
+        label="Log-bound")
+ax.scatter('search_space', 'time_to_cmp', data=seetbound, color=cmc.batlowS(2),
+        label="Seet-bound")
+
+ax.set_xlabel("Search Space")
+ax.set_ylabel("Time to Compute")
+
+fig.legend(loc='outside right center', fontsize='small')
+
+fig.savefig(figs_path + "/method_comparison.svg")
